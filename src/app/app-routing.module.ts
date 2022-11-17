9a51039b52e1fedd16dff256c8de4f425c593ba1
Matias Nicolas Michelli <proga2332@gmail.com>
2022-11-17 01:12:56 -0300

    fix details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
-    path: 'sports',
+    path: 'bitcoins',
     component: MoreNewsComponent
   },
   { path: 'tecnologia',  component: TechComponent },
